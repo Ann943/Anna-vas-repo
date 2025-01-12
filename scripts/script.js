300aa9f11dd3a7156a01842553b03927c12790fc
Ann943 <ann.vasiliva@ukr.net>
2025-01-12 22:00:49 +0200

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+document.querySelector("#facebook").addEventListener("click", function () {
+  window.location = "https://www.facebook.com/p/Louise-Joy-Brown-100000335394185/";
+});
+document.querySelector("#instagram").addEventListener("click", function () {
+  window.location = "https://www.instagram.com/louisejoybrown";
+});
+document.querySelector("#twitter").addEventListener("click", function () {
+  window.location = "https://x.com/louisejoybrown";
+});
+document.querySelector("#wikipedia").addEventListener("click", function () {
+  window.location = "https://en.wikipedia.org/wiki/Louise_Brown";
+});
